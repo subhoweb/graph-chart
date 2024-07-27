601d5935f9f2376f6b44c76d605877e096451394
Subhojyoti <Subhojyoti@gmail.com>
2024-07-27 09:38:26 +0530

    WIP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { GraphChartBar } from './components/GraphChartBar';
+import {useState} from 'react';
 
 function App() {
+  const[showGraph, setShowGraph] = useState(false)
+  const toggleButton=()=>{
+    setShowGraph(!showGraph);
+    console.log("Clickedd");
+  }
   return (
     <div className="App">
       <p>Graph Chart Application.</p>
+      <button onClick={toggleButton}>Toggle</button>
+      {showGraph ? <GraphChartBar/> : null}
     </div>
   );
 }
---
+++ src/components/GraphChartBar.jsx
@@ -0,0 +1,14 @@
+import "./chart.css"
+import CHART_DATA from "../data/APIdata"
+
+export const GraphChartBar = () => {
+  return(
+    <div className="chart-container">
+        <p>This is Graph Chart </p>
+
+
+    </div>
+    
+  )
+ 
+};
